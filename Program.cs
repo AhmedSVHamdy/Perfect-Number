88f21477c22155d01c6ff577a4e44725fcd93348
AhmedSVHamdy <120645491+AhmedSVHamdy@users.noreply.github.com>
2022-12-18 22:42:34 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,22 +10,16 @@
             int num2 = int.Parse(Console.ReadLine());
             Console.WriteLine("Perfect Resualt:");
             {
-                if (num1 >= 0) if(num2 >= 6) if(num1 < 7 )
-                {
-
+                if (num1 >= 0 && num2 >= 6 && num1 < 7)
                     Console.WriteLine("6");
-                }
-
-                if (num2 >= 28) if(num2 <= 495) if(num1 < 29)
-                {
-                   
+                if (num2 >= 28 && num1 <= 28)
                     Console.WriteLine("28");
-                }
-                if (num2 >= 496) if(num2 <= 8127) if( num1 < 49710)
-                {
-                        Console.WriteLine("496");
-                }
-
+                if (num2 >= 496 && num1 <= 496)
+                    Console.WriteLine("496");
+                if (num2 >= 8128 && num1 <= 8128)
+                    Console.WriteLine("8128");
+                if (num2 >= 33550336 && num1 <= 33550336)
+                    Console.WriteLine("33550336");
             }
 
 
